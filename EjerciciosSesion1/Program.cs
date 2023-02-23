1bb965b06b42a10713fb71337206399c258f78dc
AnaBelenS <63015324+AnaBelenS@users.noreply.github.com>
2023-02-23 22:33:55 +0100

    Add files via upload

---
+++ EjerciciosSesion1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* EJERCICIO 1
+ * Escribe un programa que reciba tu nombre y lo escriba por consola.
+ */
+
+Console.WriteLine("¿Cómo te llamas?");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
+
+
+// EJERCICIO 2
+// Escribe un programa que tome la hora y la escriba por pantalla.
+Console.WriteLine("Introduce la hora");
+String hora = Console.ReadLine();
+Console.WriteLine("Introduce los minutos");
+String minutos = Console.ReadLine();
+Console.WriteLine("Introduce los segundos");
+String segundos = Console.ReadLine();
+
+Console.WriteLine("Son las " + hora + ":" + minutos + ":" + segundos);
